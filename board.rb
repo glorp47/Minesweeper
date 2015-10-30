d08c7187b3673654369e14b6e3a1186c94f283f7
Haris Qureshi <haris.a.qureshi@gmail.com>
2015-10-30 12:05:58 -0700

    second commit: board almost done

---
+++ board.rb
@@ -0,0 +1,42 @@
+require './tile'
+
+class Board
+attr_accessor :grid
+
+def initialize(size = 9, bombs = 8)
+  @grid = Array.new(size) {Array.new(size)}
+  fill_board(bombs)
+end
+
+def fill_board(bombs)
+  #made array of values for Tiles, include a given number of bombs
+  #then shuffled the array to randomize bomb locations
+  cell_array = Array.new(@grid.size ** 2) {0}
+  0.upto(bombs) do |i|
+    cell_array[i] = :b
+  end
+  cell_array.shuffle!
+  p cell_array
+
+  #go through the grid and add a Tile to each spot, which the value
+  #from the cell_array
+  counter = 0
+  @grid.each_with_index do |row, row_idx|
+    row.each_with_index do |cell, col_idx|
+      @grid[row_idx][col_idx] = Tile.new(cell_array[counter], self, [row_idx, col_idx])
+      counter += 1
+    end
+  end
+end
+
+def render
+  puts " #{(0...size).to_a.join(" ")}"
+end
+
+
+
+
+
+
+
+end
--- tile.rb
+++ tile.rb
@@ -21,12 +21,24 @@ attr_reader :value, :flagged, :revealed
   end
 
   def neighbors
-    gives us values from all adjacent tiles on the board
+    # gives us values from all adjacent tiles on the board
+    # returns an array of the values
+    possible_moves = [-1, -1, 0, 1, 1].permutation(2).to_a
+    result = []
+    possible_moves.each do |delta|
+      result << [@pos[0]+ delta[0], @pos[1]+delta[1]]
+    end
+    result.select! {|move| move[0].between?(0,8) && move[1].between?(0,8)}
+    result.map{|position| @board.grid[position[0]][position[1]].value}
   end
 
   def neighbors_bomb_count
     neighbors.count(:b)
   end
+
+  # def inspect
+  #   @value
+  # end
 
 
 end
